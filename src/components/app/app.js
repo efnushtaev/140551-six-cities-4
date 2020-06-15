ac3247e78663d015d4b3e56756937d93ddf59a6d
efnushtaev <efnushtaev@gmail.com>
2020-06-15 14:24:33 +0300

    PropTypes regulations added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,17 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import Main from '../main/main';
+import PropTypes from 'prop-types';
 
 const App = (props) => {
-  const {offersCount} = props;
-  return <Main offersCount={offersCount} />;
+  const {offersCount, namePlaceCard} = props;
+  return <Main
+    offersCount={offersCount}
+    namePlaceCard={namePlaceCard} />;
+};
+
+App.propTypes = {
+  offersCount: PropTypes.number.isRequired,
+  namePlaceCard: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
 const OFFERS_COUNT = 137;
+const NAME_PLACE_CARD = [`luxurious apartment at great location`, `Wood and stone place`];
 
 ReactDOM.render(
-    <App offersCount={OFFERS_COUNT}/>,
+    <App
+      offersCount={OFFERS_COUNT}
+      namePlaceCard={NAME_PLACE_CARD} />,
     document.getElementById(`root`)
 );
