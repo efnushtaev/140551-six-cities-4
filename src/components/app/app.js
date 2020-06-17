0e700cd00a4e3bd33beebee9b77d2f8ff288e6c1
efnushtaev <efnushtaev@gmail.com>
2020-06-17 14:27:19 +0300

    snapshot tests added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,21 @@
+/* eslint-disable no-debugger */
 import React from 'react';
 import Main from '../main/main';
 import PropTypes from 'prop-types';
 
-const App = (props) => {
-  const {offersCount, namePlaceCard} = props;
+const App = ({offersCount, placeCardNames, onTitleClick}) => {
+
   return <Main
     offersCount={offersCount}
-    namePlaceCard={namePlaceCard} />;
+    placeCardNames={placeCardNames}
+    onTitleClick={onTitleClick}
+  />;
 };
 
 App.propTypes = {
   offersCount: PropTypes.number.isRequired,
-  namePlaceCard: PropTypes.arrayOf(PropTypes.string).isRequired
+  placeCardNames: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onTitleClick: PropTypes.func.isRequired
 };
 
 export default App;
