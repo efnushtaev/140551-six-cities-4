db7f3dca4203789e1742789db67ba65e30d45121
efnushtaev <efnushtaev@gmail.com>
2020-06-25 13:06:18 +0300

    App updated

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,21 +1,10 @@
 /* eslint-disable no-debugger */
 import React from 'react';
 import Main from '../main/main';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-const App = ({offersCount, placeCardNames, onTitleClick}) => {
-
-  return <Main
-    offersCount={offersCount}
-    placeCardNames={placeCardNames}
-    onTitleClick={onTitleClick}
-  />;
-};
-
-App.propTypes = {
-  offersCount: PropTypes.number.isRequired,
-  placeCardNames: PropTypes.arrayOf(PropTypes.string).isRequired,
-  onTitleClick: PropTypes.func.isRequired
+const App = () => {
+  return <Main/>;
 };
 
 export default App;
