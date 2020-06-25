03374c5c726b69945dd195483ed10550de1cf6b5
efnushtaev <efnushtaev@gmail.com>
2020-06-25 13:06:55 +0300

    tests updated

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -3,19 +3,10 @@ import renderer from 'react-test-renderer';
 import App from './app';
 import {describe, expect, it} from '@jest/globals';
 
-const OFFERS_COUNT = 3;
-const placeCardNames = [`first-name`, `second-name`];
-const onTitleClick = jest.fn();
-
-
 describe(`appTest`, () => {
   it(`App should be rendered correct`, () => {
     const tree = renderer
-      .create(<App
-        offersCount={OFFERS_COUNT}
-        placeCardNames={placeCardNames}
-        onTitleClick={onTitleClick}
-      />)
+      .create(<App/>)
       .toJSON();
 
     expect(tree).toMatchSnapshot();
--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -3,18 +3,10 @@ import renderer from 'react-test-renderer';
 import Main from './main';
 import {describe, expect, it} from '@jest/globals';
 
-const OFFERS_COUNT = 3;
-const placeCardNames = [`first-name`, `second-name`];
-const onTitleClick = jest.fn();
-
 describe(`mainTest`, () => {
   it(`Main should be rendered correct`, () => {
     const tree = renderer
-      .create(<Main
-        offersCount = {OFFERS_COUNT}
-        placeCardNames = {placeCardNames}
-        onTitleClick = {onTitleClick}
-      />);
+      .create(<Main/>);
 
     expect(tree).toMatchSnapshot();
   });
