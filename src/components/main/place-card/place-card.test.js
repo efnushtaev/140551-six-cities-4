0959d909932908c9ab7733e3c4b314581599cfac
efnushtaev <efnushtaev@gmail.com>
2020-06-25 14:02:52 +0300

    tests realized

---
+++ src/components/main/place-card/place-card.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PlaceCard from './place-card';
+import renderer from 'react-test-renderer';
+import {expect, it} from '@jest/globals';
+
+const mock = {
+  id: 1,
+  srcLink: `#`,
+  picture: `img/apartment-01.jpg`,
+  info: {
+    price: 120,
+    raiting: 4,
+    name: `Beautiful & luxurious apartment at great location`,
+    type: `Apartment`
+  }
+};
+
+it(`PlaceCard renders correctly`, () => {
+  const handlePlaceCardMouseOver = jest.fn();
+  const tree = renderer
+      .create(<PlaceCard
+        mock={mock}
+        onPlaceCardMouseOver={handlePlaceCardMouseOver}/>)
+      .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
