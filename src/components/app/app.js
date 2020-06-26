6bba49105aba4d8c98d5266bbf971c6eb8946568
efnushtaev <efnushtaev@gmail.com>
2020-06-26 14:01:55 +0300

    route added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-debugger */
 import React from 'react';
 import Main from '../main/main';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
 const App = () => {
-  return <Main/>;
+  return <BrowserRouter>
+    <Switch>
+      <Route exact path={`/`} render={() => <Main/>} />
+    </Switch>
+  </BrowserRouter>;
 };
 
 export default App;
