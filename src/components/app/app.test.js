b230366bebbe40705e16b0fd8047f036fa318b3f
efnushtaev <efnushtaev@gmail.com>
2020-06-26 14:02:13 +0300

    test updated

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import App from './app';
+import Main from './../main/main';
 import {describe, expect, it} from '@jest/globals';
+import {MemoryRouter} from 'react-router-dom';
 
 describe(`appTest`, () => {
   it(`App should be rendered correct`, () => {
     const tree = renderer
-      .create(<App/>)
+      .create(
+          <MemoryRouter
+            initialEntries={[{pathname: `/`}]}
+          >
+            <Main/>
+          </MemoryRouter>)
       .toJSON();
 
     expect(tree).toMatchSnapshot();
