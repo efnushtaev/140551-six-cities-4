09b5a68b3f13219dbf9591e5ffc5ef1659c524c1
efnushtaev <efnushtaev@gmail.com>
2020-06-28 19:52:46 +0300

    updated

--- src/components/main/places-list/places-list.js
+++ src/components/main/places-list/places-list.js
@@ -5,16 +5,16 @@ import PlaceCard from '../place-card/place-card';
 const PlacesList = ({mock, onPlaceCardMouseOver}) => {
   return (
     mock.map((e) => {
-      return <div className="cities__places-list places__list tabs__content" key={mock.id}>
+      return <div className="cities__places-list places__list tabs__content" key={e.id}>
         <PlaceCard mock={e} onPlaceCardMouseOver={onPlaceCardMouseOver}/>
       </div>;
     })
   );
 };
 
-PlacesList.PropTypes = {
+PlacesList.propTypes = {
   onPlaceCardMouseOver: PropTypes.func.isRequired,
-  mock: PropTypes.shape({
+  mock: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number.isRequired,
     srcLink: PropTypes.string.isRequired,
     picture: PropTypes.string.isRequired,
@@ -24,7 +24,7 @@ PlacesList.PropTypes = {
       name: PropTypes.string.isRequired,
       type: PropTypes.string.isRequired,
     })
-  })
+  }))
 };
 
 export default PlacesList;
