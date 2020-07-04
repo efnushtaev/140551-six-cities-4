19a59abc1fe1abece5d57519e265e883fb3684d9
efnushtaev <efnushtaev@gmail.com>
2020-07-04 23:16:42 +0300

    map replaced to leaflet-react

--- src/components/main/map/map.js
+++ src/components/main/map/map.js
@@ -1,63 +1,43 @@
 import React from 'react';
-import leaflet from 'leaflet';
 import PropTypes from 'prop-types';
+import {Map, Marker, Popup, TileLayer} from 'react-leaflet';
 
-class Map extends React.PureComponent {
+class MapContainer extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       city: [52.38333, 4.9],
       zoom: 12,
-      offerCords: [[52.3709553943508, 4.89309666406198], [52.3909553943508, 4.929309666406198]]
+      offerCords: [
+        [52.3709553943508, 4.89309666406198],
+        [52.3909553943508, 4.929309666406198]
+      ]
     };
-  }
-
-  _initLeaflet(city, zoom) {
-    const map = leaflet.map(`map`, {
-      center: city,
-      zoom,
-      zoomControl: false,
-      marker: true
-    });
-    map.setView(city, zoom);
-    this._leafletMapStyling(map);
-    this._mapIconRender(map, this.state.offerCords);
-  }
-
-  _leafletMapStyling(map) {
-    leaflet
-    .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-    })
-    .addTo(map);
-  }
-
-  _mapIconRender(map, offerCords) {
-    const icon = leaflet.icon({
-      iconUrl: `img/pin.svg`,
-      iconSize: [30, 30]
-    });
-
-    for (let i = 0; i <= offerCords.length; i++) {
-      leaflet
-        .marker(offerCords[i], {icon})
-        .addTo(map);
-    }
-  }
-
-  componentDidMount() {
-    this._initLeaflet(this.state.city, this.state.zoom);
+    this.map = null;
   }
 
   render() {
-    return <div className="cities__map" id="map"></div>;
+    const {city, zoom, offerCords} = this.state;
+    return <div className="cities__map">
+      <Map style={{height: `100%`, width: `100%`}} center={city} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          attribution={`&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`}
+        />
+        {offerCords.map((e) => {
+          return <Marker key={e} position={e}>
+            <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+          </Marker>;
+        })}
+      </Map>
+    </div>;
   }
 }
 
 Map.propTypes = {
   city: PropTypes.array,
-  zoom: PropTypes.number.isRequired,
+  zoom: PropTypes.number,
   offerCords: PropTypes.arrayOf(PropTypes.array),
 };
 
-export default Map;
+export default MapContainer;
