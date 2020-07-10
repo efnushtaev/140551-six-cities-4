0d05e28305907a5a9d2c313b978d6bd2bbe062eb
efnushtaev <efnushtaev@gmail.com>
2020-07-10 15:08:14 +0300

    selectors added

---
+++ src/redux/selectors/offer-selectors.js
@@ -0,0 +1,13 @@
+export const getOffers = (state) => {
+  return state.offers[`offers`].filter((el) => {
+    return el.city.name === state.offers.currentCity;
+  });
+}
+
+export const getCity = (state) => {
+  return state.offers.city;
+}
+
+export const getCurrentCity = (state) => {
+  return state.offers.currentCity;
+} 
