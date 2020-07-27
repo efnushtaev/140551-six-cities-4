5ec35025d00720ea2476bc620b55404019a9a47b
efnushtaev <efnushtaev@gmail.com>
2020-07-27 14:43:44 +0300

    response status const added

--- src/constants/const.js
+++ src/constants/const.js
@@ -4,3 +4,9 @@ export const SortType = {
   priceHighToLow: `Price: high to low`,
   topRatedFirst: `Top raited first`
 };
+
+export const ResponsedStatus = {
+  SUCCESS: 200,
+  ERROR: 400,
+  UNAUTHORIZED: 401
+}
